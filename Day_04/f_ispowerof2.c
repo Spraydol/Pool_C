7662ff56d95743d0d7a990fbb2fec0a8ee53f1f0
jvvvjvvn <jvvvjnnv@gmail.com>
2025-11-27 23:14:31 +0100

    Day 4 Done

---
+++ Day_04/f_ispowerof2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ {
+    int n, i, somme = 0;
+
+    printf("Entrer un entier n : ");
+    scanf("%d", &n);
+
+    for (i = 0; i <= n; i++) {
+        somme += i;
+    }
+
+    printf("La somme de 0 jusqu'à %d est : %d\n", n, somme);
+    return 0;
+} 
---
+++ Day_04/f_tableau_multi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+	int n , i ;
+	printf("entrer la valeur de n : ");
+	scanf("%d" , &n);
+	for (i = 1 ; i<=10 ; i++){
+		printf("%d * %d = %d \n" , n , i , n*i);
+	}
+	return 0 ;
+}
